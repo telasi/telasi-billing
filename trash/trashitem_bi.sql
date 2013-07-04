b53835162d63ebe0e3ce3f69815148a92cfa5e60
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-03 23:55:03 -0700

    Update trashitem_bi.sql
    old version of trashitem_bi trigger

--- trash/trashitem_bi.sql
+++ trash/trashitem_bi.sql
@@ -1,5 +1,5 @@
-
-  CREATE OR REPLACE TRIGGER "BS"."TRASHITEMID_BI" 
+create or replace
+TRIGGER bs.TRASHITEMID_BI
  BEFORE 
  INSERT
  ON bs.TRASHITEM
@@ -32,17 +32,19 @@ THEN
 
     IF opertp = 2 AND iscorr = 0 THEN
       -- gadaxda
-      newamount := - :NEW.amount;
+      newamount := - ROUND(:NEW.amount, 2);
     ELSE
-      newamount := + :NEW.amount;
+      newamount := + ROUND(:NEW.amount, 2);
     END IF;
 
     IF :NEW.operationid != 208 THEN
 
       SELECT
-        balance, old_balance, tech_balance, curr_balance
+        ROUND(NVL(balance, 0), 2), ROUND(NVL(old_balance, 0), 2),
+        ROUND(NVL(tech_balance, 0), 2), ROUND(NVL(curr_balance, 0), 2)
       INTO
-        :NEW.balance, :NEW.old_balance, :NEW.tech_balance, l_old_curr_balance
+        :NEW.balance, :NEW.old_balance,
+        :NEW.tech_balance, l_old_curr_balance
       FROM
         trashcustomer
       WHERE custkey = :NEW.custkey;
@@ -50,21 +52,30 @@ THEN
       -- payment (use normilized current balance)
       IF
         :NEW.operationid IN (12, 35, 150, 20) OR -- payment
-        (:NEW.operationid IN (53) AND newamount < 0) -- internal offset < 0 (do like payment)
+        (:NEW.operationid IN (53) AND newamount < 0) OR -- internal offset < 0 (do like payment)
+        (:NEW.operationid IN (227, 230))
       THEN
-        l_old_curr_balance := BS.trash_manager_2007_2.get_normalized_currbalance2(:NEW.custkey);
+        l_old_curr_balance := ROUND(BS.trash_manager_2007_2.get_normalized_currbalance2(:NEW.custkey), 2);
       END IF;
 
       l_cust_balance      := NVL(:NEW.balance,      0) + newamount;
 
-      IF :NEW.operationid IN (19, 100, 213, 223) -- Zveli valis gadatana!!!
+      IF :NEW.operationid IN (19, 100, 213, 223, 80, 82, 228) -- Zveli valis gadatana!!!
       THEN
         l_cust_old_balance   := NVL(:NEW.old_balance,  0) + newamount;
         l_cust_tech_balance  := NVL(:NEW.tech_balance, 0) + newamount;
+        
         IF l_cust_old_balance < 0
         THEN
           l_cust_old_balance := 0;
         END IF;
+      ELSIF :NEW.operationid IN (73, 129, 229, 600, 601, 602, 603, 131, 132) -- MXOLOD mimdinare valis cvlileba!!!
+      THEN
+        ------------------------------------------------------------------------
+        -- leilas werili!!! 30-Aug-2011 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ------------------------------------------------------------------------
+        l_cust_old_balance   := :NEW.old_balance;
+        l_cust_tech_balance  := :NEW.tech_balance;
       -- Zveli valis dakreditebis gasworeba
       ELSIF :NEW.operationid IN (trash_manager_2007_2.OLD_BALANCE_GE_0_OPERATION) -- Zveli balansis ganuleba (AMOUNT=0!)
       THEN
@@ -99,10 +110,11 @@ THEN
 
       UPDATE trashcustomer
       SET
-        balance      = l_cust_balance,
-        old_balance  = l_cust_old_balance,
-        tech_balance = l_cust_tech_balance,
-        curr_balance = (l_cust_balance - l_cust_old_balance)
+        balance          = ROUND(l_cust_balance, 2),
+        old_balance      = ROUND(l_cust_old_balance, 2),
+        tech_balance     = ROUND(l_cust_tech_balance, 2),
+        curr_balance     = (ROUND(l_cust_balance, 2) - ROUND(l_cust_old_balance, 2)),
+        discon_balance   = (ROUND(l_cust_balance, 2) - ROUND(l_cust_old_balance, 2))
       WHERE
         custkey = :NEW.custkey;
 
@@ -119,5 +131,3 @@ THEN
 END IF;
 
 END;
-
-ALTER TRIGGER "BS"."TRASHITEMID_BI" ENABLE
